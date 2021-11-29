696d301372d7a8584f67472c371ae5d2e60d1f00
evatracey7 <87449532+evatracey7@users.noreply.github.com>
2021-11-29 12:04:08 -0500

    string assigment

---
+++ String_Project/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+
+namespace String_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "John ";
+            string middleName = "Michael ";
+            string lastName = "Doe";
+            StringBuilder fullName = new StringBuilder();
+            fullName.Append(string.Concat(firstName.ToUpper(), middleName.ToUpper(), lastName.ToUpper()));
+            Console.WriteLine(fullName);
+            Console.ReadLine();
+        }
+    }
+}
