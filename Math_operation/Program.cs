52855465187f72a71f69dd92dfd1329f9fdf9522
evatracey7 <87449532+evatracey7@users.noreply.github.com>
2021-11-29 11:56:59 -0500

    math operation

---
+++ Math_operation/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Math_operation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to multiply it by 50");
+            int numMultiply = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(numMultiply * 50);
+
+            Console.WriteLine("Enter a number to add it by 25");
+            int numAdd = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(numAdd + 25);
+
+            Console.WriteLine("Enter a number to divide it by 12.5");
+            float numDivide = float.Parse(Console.ReadLine());
+            Console.WriteLine(numDivide / 12.5);
+
+            Console.WriteLine("Enter a number to check whether it is greater than 50");
+            int numGreater = Convert.ToInt32(Console.ReadLine());
+            bool numBool = numGreater > 50;
+            Console.WriteLine(numBool);
+
+            Console.WriteLine("Enter a number to check its remainder");
+            int numRemainder = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(numRemainder % 7);
+        }
+    }
+}
