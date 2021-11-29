ea0680a1dba9de5c07de65693cbe4013df6d84af
evatracey7 <87449532+evatracey7@users.noreply.github.com>
2021-11-29 11:59:46 -0500

    boolean logic assigment

---
+++ Boolean_Project/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Boolean_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Have you ever had a DUI? type [true/false]");
+            string DUI = Console.ReadLine();
+
+            Console.WriteLine("How many speeding tickets do you have");
+            int speedingTickets = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Qualify");
+            bool qualify = (age >= 15 && DUI == "false" && speedingTickets <= 3);
+
+            Console.WriteLine(qualify);
+        }
+    }
+}
