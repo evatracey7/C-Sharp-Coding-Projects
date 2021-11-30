7646e58c3d92f89fa4f8b474446876089c41cba9
evatracey7 <87449532+evatracey7@users.noreply.github.com>
2021-11-30 11:13:24 -0500

    updated loop assignment

--- loop_project/Program.cs
+++ loop_project/Program.cs
@@ -10,31 +10,32 @@ namespace loop_project
             int number = Convert.ToInt32(Console.ReadLine());
             bool isGuessed = number == 0;
 
-            do
+            while (number >= 0)
             {
-                switch (number)
+                do
                 {
-                    case 50:
-                        Console.WriteLine("You entered 50.Try again");
-                        Console.WriteLine("Guess the number?");
-                        number = Convert.ToInt32(Console.ReadLine());
-                        break;
+                    switch (number)
+                    {
+                        case 50:
+                            Console.WriteLine("You entered 50.Try again");
+                            Console.WriteLine("Guess the number?");
+                            number = Convert.ToInt32(Console.ReadLine());
+                            break;
 
-                    case 100:
-                        Console.WriteLine("You entered 100.Try again");
-                        Console.WriteLine("Guess the number?");
-                        number = Convert.ToInt32(Console.ReadLine());
-                        break;
+                        case 100:
+                            Console.WriteLine("You entered 100.Try again");
+                            Console.WriteLine("Guess the number?");
+                            number = Convert.ToInt32(Console.ReadLine());
+                            break;
 
-                    default:
-                        Console.WriteLine("You neither entered 50 nor 100");
-                        number = Convert.ToInt32(Console.ReadLine());
-                        break;
-
+                        default:
+                            Console.WriteLine("You neither entered 50 nor 100");
+                            number = Convert.ToInt32(Console.ReadLine());
+                            break;
+                    }
                 }
+                while (!isGuessed);
             }
-            while (!isGuessed);
-
             Console.Read();
         }
     }
